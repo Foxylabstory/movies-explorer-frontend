d06db10412ca4c10a0dbab4129c80b78e836f742
Foxylab <rushan.shakirzianov@gmail.com>
2022-09-17 09:23:54 +0300

    feat: finish AboutProject

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -2,20 +2,20 @@ import './AboutProject.css';
 
 function AboutProject(props) {
     return (
-        <section className="ol about-project">
-            <div className="ol about-project__container">
+        <section className="about-project">
+            <div className="about-project__container">
                 <h2 className="about-project__header_main">О проекте</h2>
-                <div className='ol about-project__details'>
-                    <h3 className="ol about-project__header">Дипломный проект включал 5 этапов</h3>
-                    <h3 className="ol about-project__header">На выполнение диплома ушло 5 недель</h3>
-                    <p className="ol about-project__content">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-                    <p className="ol about-project__content">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                <div className='about-project__details'>
+                    <h3 className="about-project__header">Дипломный проект включал 5 этапов</h3>
+                    <h3 className="about-project__header">На выполнение диплома ушло 5 недель</h3>
+                    <p className="about-project__content">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                    <p className="about-project__content">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
                 </div>
-                <div className='ol about-project__timeline'>
-                <div className="ol about-project__block">1 неделя</div>
-                    <div className="ol about-project__block">4 недели</div>
-                    <div className="ol about-project__block about-project__block_witout-borders">Back-end</div>
-                    <div className="ol about-project__block about-project__block_witout-borders">Front-end</div>
+                <div className='about-project__timeline'>
+                <div className="about-project__block">1 неделя</div>
+                    <div className="about-project__block">4 недели</div>
+                    <div className="about-project__block about-project__block_witout-borders">Back-end</div>
+                    <div className="about-project__block about-project__block_witout-borders">Front-end</div>
                 </div>
             </div>
         </section>
