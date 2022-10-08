e4d1dc6d29146d0f03ccac5757034217c36bbf70
Foxylab <rushan.shakirzianov@gmail.com>
2022-10-08 15:37:24 +0300

    cleanup warnings

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import './SavedMovies.css';
 /* import { Link } from 'react-router-dom'; */
 import Header from "../BaseComponents/Header/Header";
